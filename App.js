4ee35c5ec8a872304fc8f93539f50a61192e9f92
Nefferou <fertijulien@gmail.com>
2024-03-28 10:30:17 +0100

    App change

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import {signup} from "./auth_signup_password";
 
 export default function App() {
+  const [email, onChangeEmail] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Email</Text>
+      <TextInput
+      style={style.input}
+      onChangeText={onChangeEmail}
+      value={email}>
+      <Text>Password</Text>
+      <TextInput
+      style={style.input}
+      onChangeText={onChangePassword}
+      value={password}
+      secureTextEntry={true}></TextInput>
+      </TextInput>
+      <Button title="Signup !" onPress={() => signup(email, password)} />
     </View>
   );
 }
