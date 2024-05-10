5c21d9885e8e998fe1b52cab3d1fe7a1ec22d1de
Nefferou <fertijulien@gmail.com>
2024-05-10 18:16:09 +0200

    add config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,16 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCYspJDvQQEFBIqh9coPacmhv8aWB7fpAk",
   authDomain: "web-cloud-6cd23.firebaseapp.com",
   projectId: "web-cloud-6cd23",
   storageBucket: "web-cloud-6cd23.appspot.com",
   messagingSenderId: "297674886054",
-  appId: "1:297674886054:web:5fb5c6cacff8f712e69d67",
-  measurementId: "G-114J99S47X"
+  appId: "1:297674886054:web:5fb5c6cacff8f712e69d67"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
